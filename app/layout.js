e8b5f60f9f861a2da5508f00646e44424f272f83
ltxstudio <ltxstudio.info@gmail.com>
2024-11-13 22:06:13 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -40,6 +40,12 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="robots" content="index, follow" />
         <meta name="theme-color" content="#ffffff" />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Markdown Converter" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className={`${inter.className} ${poppins.className}`}>
         {children}
